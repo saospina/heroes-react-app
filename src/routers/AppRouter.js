a155ef8ef278292b2e3d43aa6c8a7fb03253b7c0
saospina <sergos0617@gmail.com>
2021-03-23 14:48:15 -0500

    creating public and private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { BrowserRouter, Switch } from 'react-router-dom'
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { AuthContext } from '../auth/AuthContext';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
+
+    const { userState } = useContext(AuthContext);
     return (
         <BrowserRouter>
             <div>
                 <Switch>
-                    <Route exact path="/login" component={LoginScreen} />
-                    <Route path="/" component={DashboardRoutes} />
+                    <PublicRoute
+                        exact path="/login"
+                        component={LoginScreen}
+                        isAuthenticated={userState.logged}
+                    />
+                    <PrivateRoute
+                        path="/"
+                        component={DashboardRoutes}
+                        isAuthenticated={userState.logged}
+                    />
                 </Switch>
             </div>
         </BrowserRouter>
